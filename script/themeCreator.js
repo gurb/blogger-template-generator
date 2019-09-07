c06ea8747010f4c02954e2288e3de60952bef669
Gürbüz Uðurgül <herbokvar@outlook.com>
2019-09-07 16:23:45 +0300

    add project

---
+++ script/themeCreator.js
@@ -0,0 +1,7 @@
+function Section(idName, className, max_widgets, show_add_element){
+    this.idName = idName;
+    this.className = className;
+    this.max_widgets = max_widgets;
+    this.show_add_element = show_add_element; 
+}
+
